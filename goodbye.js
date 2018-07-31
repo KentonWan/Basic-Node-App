1917444f00d33bf9a1b97a9b84b9d0ae9b64b93c
Kenton Wan <kenton.wan@gmail.com>
2018-07-31 10:39:26 -0700

    Basic Node App Assignment Completed

---
+++ goodbye.js
@@ -0,0 +1,15 @@
+exports.goodbyeEnglish = () => {
+  console.log("Goodbye")
+};
+
+exports.goodbyeJapanese = () => {
+  console.log("Sayanora")
+};
+
+exports.goodbyeSpanish = () => {
+  console.log("Adios")
+};
+
+exports.goodbyeIcelandic = () => {
+  console.log("bless")
+};
---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require("./greeting.js");
+const goodbye = require("./goodbye.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbye.goodbyeEnglish();
+
+goodbye.goodbyeJapanese();
+
+goodbye.goodbyeSpanish();
+
+goodbye.goodbyeIcelandic();
